50128729ae7a803d7f77f374627211bc613742e8
Rushi <rushikeshshelar.cs@gmail.com>
2024-01-06 17:23:48 +0530

    added Hostname to display images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "oaidalleapiprodscus.blob.core.windows.net",
+            },
+        ],
+    },
+    experimental: {
+        serverActions: true,
+    },
+};
 
 module.exports = nextConfig

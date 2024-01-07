7e51d7aa5e2cff8179944cff27b360bded5def2a
Rushi <rushikeshshelar.cs@gmail.com>
2024-01-07 19:25:05 +0530

    Removed uncessary Imports

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -7,7 +7,6 @@ import { useRouter } from "next/navigation";
 import { Music } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
 
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/heading";

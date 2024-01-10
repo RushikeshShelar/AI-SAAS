9adfbec3520b7625c608af418dcdc9ce0d627c3b
Rushi <rushikeshshelar.cs@gmail.com>
2024-01-10 23:57:50 +0530

    State management Components & Providers

---
+++ components/pro-modal.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import {
+    Check,
+    Code,
+    Image,
+    MessageSquare,
+    Music,
+    Video,
+    Zap
+} from "lucide-react";
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle
+} from "@/components/ui/dialog";
+
+import { useProModal } from "@/hooks/use-pro-modal";
+import { Badge } from "@/components/ui/badge";
+import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+const tools = [
+    {
+        label: "Conversation",
+        icon: MessageSquare,
+        color: 'text-violet-500',
+        bgColor: 'bg-violet-500/10',
+    },
+    {
+        label: "Music Generation",
+        icon: Music,
+        color: 'text-emerald-500',
+        bgColor: 'bg-emerald-500/10',
+    },
+    {
+        label: "Image Generation",
+        icon: Image,
+        color: 'text-pink-700',
+        bgColor: 'bg-pink-500/10',
+    },
+    {
+        label: "Video Generation",
+        icon: Video,
+        color: 'text-orange-700',
+        bgColor: 'bg-orange-700/10',
+    },
+    {
+        label: "Code Generation",
+        icon: Code,
+        color: 'text-green-700',
+        bgColor: 'bg-green-700/10',
+    },
+]
+
+const ProModal = () => {
+
+    const proModal = useProModal()
+
+    return (
+        <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
+            <div className="flex items-center gap-x-2 font-bold text-xl">
+              Upgrade to Genius
+              <Badge variant="premium" className="uppercase text-sm py-1">
+                pro
+              </Badge>
+            </div>
+          </DialogTitle>
+          <DialogDescription className="text-center pt-2 space-y-2 text-zinc-900 font-medium">
+            {tools.map((tool) => (
+              <Card key={tool.label} className="p-3 border-black/5 flex items-center justify-between">
+                <div className="flex items-center gap-x-4">
+                  <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                    <tool.icon className={cn("w-6 h-6", tool.color)} />
+                  </div>
+                  <div className="font-semibold text-sm">
+                    {tool.label}
+                  </div>
+                </div>
+                <Check className="text-primary w-5 h-5" />
+              </Card>
+            ))}
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button onClick={proModal.onOpen} size="lg" variant="premium" className="w-full">
+            Upgrade
+            <Zap className="w-4 h-4 ml-2 fill-white" />
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+    );
+}
+
+export default ProModal;

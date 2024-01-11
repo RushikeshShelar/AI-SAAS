10216eda994da6d8924c7199bbc394fcd4422252
Rushi <rushikeshshelar.cs@gmail.com>
2024-01-12 00:13:01 +0530

    Added Pro modal and Subscription Button

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,6 +3,7 @@ import Sidebar from "@/components/sidebar";
 import React from "react";
 
 import { getApiLimitCount } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
 
 const DashboardLayout = async ({
     children,
@@ -11,11 +12,12 @@ const DashboardLayout = async ({
 }) => {
 
     const apiLimitCount = await getApiLimitCount();
+    const isPro = await checkSubscription();
 
     return ( 
         <div className="h-full relative">
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-900">
-                <Sidebar apiLimitCount={apiLimitCount} />
+                <Sidebar apiLimitCount={apiLimitCount} isPro={isPro} />
             </div>
             <main className="md:pl-72">
                 <Navbar />
---
+++ components/subscription-button.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import axios from "axios";
+import { Zap } from "lucide-react";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import toast from "react-hot-toast";
+
+interface SubscriptionButtonProps {
+    isPro: boolean;    
+}
+
+const SubscriptionButton = ({
+    isPro = false
+}: SubscriptionButtonProps) => {
+
+    const [loading, setLoading] = useState(false);
+
+    const onClick = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.get("/api/stripe");
+            
+            window.location.href = response.data.url;
+            
+        } catch (error) {
+            toast.error("Something went wrong.");
+        } finally{
+            setLoading(false);
+        }
+    }
+
+    return ( 
+        <Button disabled={loading} variant={isPro ? "default" : "premium"} onClick={onClick}>
+            {isPro ? "Manage Subscription" : "Upgrade"}
+            {!isPro && <Zap className="w-4 h-8 ml-2 fill-white"/>}
+        </Button>
+     );
+}
+ 
+export default SubscriptionButton;
